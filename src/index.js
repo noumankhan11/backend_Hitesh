9c444b6975bcdf199bd8c2b170cf53ab22bade5f
Nouman Khan <nomiofficial68@gmail.com>
2024-06-03 22:34:09 +0500

    connected to the database for test and other setup

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+import connectDb from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDb();
+
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`process.env.MONGODB_URI/${DB_NAME}`);
+    app.on("error", (err) => {
+      console.log("Error: ", err);
+      throw err;
+    });
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("app is listening on port : ", process.env.PORT);
+    });
+  } catch (error) {
+    console.error("error while connection to db: ", error);
+  }
+})();
+*/
