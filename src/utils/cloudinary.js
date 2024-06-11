7059a29f51a0fbdc63c385bae09057ab9c430ba2
Nouman Khan <nomiofficial68@gmail.com>
2024-06-11 23:15:00 +0500

    setup cloudinary for file upload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View Credentials' below to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFIlePath) => {
+  try {
+    if (!localFIlePath) return null;
+    //upload on cloudinary
+    const response = await cloudinary.uploader.upload(localFIlePath, {
+      resource_type: "auto",
+    });
+    // file uploaded successfully;
+    console.log("file uploaded on cloudinay successfully", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFIlePath); // romove locall file on any error
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
