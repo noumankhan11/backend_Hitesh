47201fce281884cb061f5e4593e6641c65e8da5b
Nouman Khan <nomiofficial68@gmail.com>
2024-07-02 22:38:31 +0500

    add new routes for userController

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,9 +4,17 @@ import {
   loginUser,
   logoutUser,
   refreshAccessToken,
+  changePassword,
+  getCurrentUser,
+  udpateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyJwt } from "../middleware/auth.middleware.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 const router = Router();
 
 router.route("/register").post(
@@ -29,4 +37,21 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJwt, logoutUser);
 
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJwt, changePassword);
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+router.route("/update-account").patch(verifyJwt, udpateAccountDetails);
+
+router
+  .route("/avatar")
+  .patch(verifyJwt, upload.single("avatar"), updateUserAvatar());
+
+router
+  .route("/cover-image")
+  .patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile);
+
+router.route("/history").get(verifyJwt, getWatchHistory);
+
 export default router;
